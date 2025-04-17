3dd7e0729f15e01cc24b5973bdef7cb6694dc6dd
myagkov <myagkov@inventive.ru>
2025-04-17 14:51:15 +0300

    Refactor import and generate cli commands, integrate streams

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,85 +1,36 @@
-import { readFileSync } from 'node:fs';
+import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
 import { FileReader } from './file-reader.interface.js';
-import {
-  Offer,
-  OfferType,
-  City,
-  OfferFeature,
-  UserType,
-} from '../../types/index.js';
-import { doesSatisfyEnum } from '../../helpers/index.js';
 
+const CHUNK_SIZE = 102400; // 100KB
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
-
-  constructor(
-    private readonly filename: string
-  ) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      throw new Error('File was not read');
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split('\t'))
-      .map(([
-        name,
-        description,
-        createdDate,
-        city,
-        photoPreview,
-        photos,
-        isPremium,
-        isFavorite,
-        rating,
-        type,
-        roomsCount,
-        guestCount,
-        price,
-        features,
-        user,
-        commentsCount,
-        coordinates
-      ]) => {
-        const [userName, userEmail, userAvatar, userPassword, userType] = user.split(';');
-        const [lat, lon] = coordinates.split(';');
-
-        return {
-          name,
-          description,
-          postDate: new Date(createdDate),
-          city: city as City,
-          photoPreview,
-          photos: photos.split(';'),
-          isPremium: isPremium === 'Y',
-          isFavorite: isFavorite === 'Y',
-          type: type as OfferType,
-          rating: Number.parseInt(rating, 10),
-          roomsCount: Number.parseInt(roomsCount, 10),
-          guestCount: Number.parseInt(guestCount, 10),
-          features: features.split(';').filter((feature) => doesSatisfyEnum(feature, OfferFeature)) as OfferFeature[],
-          user: {
-            avatar: userAvatar,
-            email: userEmail,
-            name: userName,
-            password: userPassword,
-            type: userType as UserType,
-          },
-          commentsCount: Number.parseInt(commentsCount, 10),
-          coordinates: {
-            lat: Number.parseFloat(lat),
-            lon: Number.parseFloat(lon),
-          },
-          price: Number.parseInt(price, 10),
-        };
-      });
+    this.emit('end', importedRowCount);
   }
 }
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -3,3 +3,6 @@ export * from './offer-feature.enum.js';
 export * from './offer.interface.js';
 export * from './city.enum.js';
 export * from './user-type.enum.js';
+export * from './user.interface.js';
+export * from './coordinates.type.js';
+export * from './mock-server-data.type.js';
